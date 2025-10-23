cd3ea63a72ce8309d671b2c5db923cf689ec3807
olsaa0 <166027576+olsaa0@users.noreply.github.com>
2025-10-23 16:15:21 +0200

    Add files via upload

---
+++ Module1/index.py
@@ -0,0 +1,27 @@
+# print('Hello World')
+
+temp=98.8
+name='Olsa'
+
+print(name)
+
+print(type(name))
+print(type(temp))
+
+print('The best student in class is ',name)
+
+x=5
+y=2
+result=x+y
+
+print(result)
+
+shoppinglist=['orange', 'apple', 9.1]
+print(shoppinglist[1])
+
+shoppinglist.append('milk')
+shoppinglist.insert(2,'lemons')
+
+shoppinglist.remove('apple')
+shoppinglist.pop(1)
+print(shoppinglist)
