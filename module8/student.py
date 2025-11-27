da9053edd9d244397ee6f64a5e2f0d68290f29d3
Student <olsaolsa30@gmail.com>
2025-11-27 16:14:30 +0100

    oop

---
+++ module8/student.py
@@ -0,0 +1,12 @@
+class Student:
+    school_name = "Digital School"
+    def __init__(self, name, age, course):
+        self.name = name
+        self.age = age
+        self.course = course
+
+student_1= Student("Alice", 15, "Python")
+student_2= Student("Bob", 16, "JavaScript")
+
+print(student_1.course)
+print(student_2.name)
