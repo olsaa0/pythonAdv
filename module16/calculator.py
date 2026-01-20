5f36bfeeac2dbf3f556924e7507fb16d97c4fe64
olsaa0 <166027576+olsaa0@users.noreply.github.com>
2026-01-20 16:11:21 +0100

    Add files via upload

---
+++ module16/calculator.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+def calculator(num1,num2,operation):
+    if operation=="Add":
+        result=num1+num2
+    if operation=="Sub":
+        result=num1-num2
+    if operation=="Multi":
+        result=num1*num2
+    if operation=="Div":
+        try:
+         result=num1/num2
+        except ZeroDivisionError:
+            result="Cannot divide by zero"
+        return result
+
+def main():
+    st.title("simple calculator")
+
+    num1=st.number_input("Enter the first number",step=1)
+
+    num2=st.number_input("Enter the second number",step=1)
+
+    operation=st.radio("Select operation",["Add","Sub","Multi","Div"])
+
+    result=calculator(num1,num2,operation)
+
+    st.write(f"Result of the {operation} of {num1} and {num2} is {result}")
+
+if __name__=="__main__":
+    main()
