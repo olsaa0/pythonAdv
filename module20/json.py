67ba63c4da64eb1281b426fbe07669041d68eccb
olsaa0 <166027576+olsaa0@users.noreply.github.com>
2026-02-10 16:02:42 +0100

    Add files via upload

---
+++ module20/json.py
@@ -0,0 +1,45 @@
+# {
+#     "name":"Kiki",
+#     "age":20,
+#     "city":"New York"
+# }
+# jsoncrack
+
+# {
+#     "name":"John Doe",
+#     "age":30,
+#     "address":{
+#         "street":"123 Main St",
+#         "city":"Prishtine",
+#         "state":"Kosove",
+#         "PostalCode":"10000"
+#     },
+#     "contacts":[
+#         {
+#         "type":"Email",
+#         "value":"johndoe@gmail.com"
+#         },
+#         {
+#             "type":"phone",
+#             "value":"444-122-4567"
+#         }
+#     ]
+# }
+
+{
+    "fruits":[
+        {
+            "name":"Aplle",
+            "color":"#FF0000",
+            "details":{
+                "type":'Pome',
+                "season":'Fall'
+            },
+            "nutrients":{
+                'calories':52,
+                'fiber':"2.4g",
+                'vitaminC':"4.6mg"
+            }
+        },
+    ]
+}
