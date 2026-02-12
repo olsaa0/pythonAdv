3e9195fce086a87ded35e84d74c3571316dfd245
olsaa0 <166027576+olsaa0@users.noreply.github.com>
2026-02-12 16:08:52 +0100

    Add files via upload
    crud

---
+++ module21/crud.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return{"message":"hello world"}
+ @app.get("/items")
+ def read_items():
+     return{"items":["item1","item2","item3"]}
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {"user_id": user_id, "name": "kiki"}
+
+ @app.post(".items/")
+ def create_item(name:str,price:float):
+    return {"item_name": name, "item_price": price}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id:int,name:str,price:float):
+    return{"item_id": item_id, "item_name":name, "item_price":price}
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id:int):
+    return{"message":f"Item{item_id}deleted"}
+
+#uvicorn main:app --reload
